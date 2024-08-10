cfb6ca9eeacf7a163e87910ff57281e50bd30913
oseji <jiade1233@gmail.com>
2024-08-10 20:29:42 +0100

    uninstalled shadcn and removed shadcn calender component from schedule section

--- src/dashboard/Schedule.tsx
+++ src/dashboard/Schedule.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import { Calendar } from "../components/ui/calendar";
-
 import calndarIcon from "../assets/calendarIcon.svg";
 
 const Schedule = () => {
-  const [date, setDate] = useState<Date | undefined>(new Date());
-
   return (
-    <section className="bg-white dark:bg-[#0F0F0F] rounded max-w-full mx-3 lg:mx-0 my-3 p-3 text-sm">
+    <section className=" w-full bg-white dark:bg-[#0F0F0F] rounded mx-3 lg:mx-0 my-3 p-3 text-sm">
       <div className="calendarHeading">
         <div className="flex flex-row items-center gap-2">
           <img src={calndarIcon} alt="calendar" />
@@ -18,13 +13,6 @@ const Schedule = () => {
           + New
         </button>
       </div>
-
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border "
-      />
     </section>
   );
 };
